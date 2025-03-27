a3ab70c12b0aec87b09ecbe20d2c358200c4854e
flyingsl0ths <randomemail33110@protonmail.com>
2025-03-27 09:24:38 -0700

    Flesh out Bubble repr.

--- src/bubbles/value.rs
+++ src/bubbles/value.rs
@@ -1,9 +1,9 @@
-use bevy::{asset::Handle, math::Vec3, sprite::ColorMaterial};
+use bevy::math::Vec3;
 
 pub struct Bubble {
-    pub color: Handle<ColorMaterial>,
     pub row: usize,
     pub column: usize,
     pub pos: Vec3,
     pub radius: f32,
+    pub edges: [usize; 6],
 }
