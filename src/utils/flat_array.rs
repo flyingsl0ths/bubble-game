e70d11fb46ec5a412a7a030f0d74302cae971ef7
flyingsl0ths <randomemail33110@protonmail.com>
2025-05-05 03:46:07 -0700

    Rename const param.

--- src/utils/flat_array.rs
+++ src/utils/flat_array.rs
@@ -3,15 +3,19 @@ use core::ops::{Index, IndexMut};
 use bevy::prelude::Resource;
 
 #[derive(Resource)]
-pub struct FlatArray<T, const N: usize>(Vec<T>);
+pub struct FlatArray<T, const C: usize>(Vec<T>);
 
-impl<T, const N: usize> FlatArray<T, N> {
+impl<T, const C: usize> FlatArray<T, C> {
+    pub fn to_index(row: usize, column: usize) -> usize {
+        row * C + column
+    }
+
     pub fn get(&self, row: usize, column: usize) -> Option<&T> {
-        self.0.get(row * N + column).or(None)
+        self.0.get(row * C + column).or(None)
     }
 
     pub fn get_mut(&mut self, row: usize, column: usize) -> Option<&mut T> {
-        self.0.get_mut(row * N + column).or(None)
+        self.0.get_mut(row * C + column).or(None)
     }
 
     pub fn new(values: Vec<T>) -> Self {
@@ -23,18 +27,18 @@ impl<T, const N: usize> FlatArray<T, N> {
     }
 }
 
-impl<T, const N: usize> Index<(usize, usize)> for FlatArray<T, N> {
+impl<T, const C: usize> Index<(usize, usize)> for FlatArray<T, C> {
     type Output = T;
 
     fn index(&self, index: (usize, usize)) -> &Self::Output {
         let (row, column) = index;
-        &self.0[row * N + column]
+        &self.0[row * C + column]
     }
 }
 
-impl<T, const N: usize> IndexMut<(usize, usize)> for FlatArray<T, N> {
+impl<T, const C: usize> IndexMut<(usize, usize)> for FlatArray<T, C> {
     fn index_mut(&mut self, index: (usize, usize)) -> &mut Self::Output {
         let (row, column) = index;
-        &mut self.0[row * N + column]
+        &mut self.0[row * C + column]
     }
 }
