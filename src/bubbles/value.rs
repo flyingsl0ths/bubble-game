17dbf400de67963a89bdae85fe05a9a08157bfae
flyingsl0ths <randomemail33110@protonmail.com>
2025-05-05 03:46:32 -0700

    Introduce bubble mesh resource holder

--- src/bubbles/value.rs
+++ src/bubbles/value.rs
@@ -1,11 +1,22 @@
-use bevy::math::Vec3;
+use bevy::{
+    asset::Handle,
+    math::Vec3,
+    prelude::{Mesh, Resource},
+};
+
+use super::colors::BubbleColors;
+
+pub const EDGE_COUNT: usize = 6;
+
+#[derive(Resource)]
+pub struct BubbleShape(pub Handle<Mesh>);
 
 pub struct Bubble {
     pub row: usize,
     pub column: usize,
     pub pos: Vec3,
-    pub radius: f32,
-    pub edges: [(usize, usize); 6],
+    pub edges: [(usize, usize); EDGE_COUNT],
+    pub color: BubbleColors,
 }
 
 impl Bubble {
